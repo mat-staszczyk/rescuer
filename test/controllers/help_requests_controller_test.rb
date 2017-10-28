cf3776f2a5804e959a4cd397bcb450792238eef7
jerryskye <jarek@siedlarz.com>
2017-10-29 00:42:57 +0200

    fixed the tests

--- test/controllers/help_requests_controller_test.rb
+++ test/controllers/help_requests_controller_test.rb
@@ -1,24 +1,32 @@
 require 'test_helper'
 
 class HelpRequestsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @help_request = FactoryBot.create(:help_request)
+    @user = @help_request.author
+  end
+
   test "should get index" do
-    get help_requests_index_url
+    get help_requests_url
     assert_response :success
   end
 
   test "should get show" do
-    get help_requests_show_url
+    get help_requests_url(@help_request)
     assert_response :success
   end
 
   test "should get edit" do
-    get help_requests_edit_url
+    get edit_help_request_url(@help_request)
     assert_response :success
   end
 
-  test "should get create" do
-    get help_requests_create_url
-    assert_response :success
+  test "should create a help request" do
+    login_as @user
+    assert_difference 'HelpRequest.count' do
+      post help_requests_url, params: { help_request: { title: 'Title', description: 'A sample description', author: @user } }
+      assert_response :success
+    end
   end
 
 end
