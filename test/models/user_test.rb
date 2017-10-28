8fa9b89edf4f67a44ff377bcd3dbdfc9bda35451
sleaz0id <mat.staszczyk@gmail.com>
2017-10-28 17:30:09 +0200

    User model tests added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+  should validate_presence_of(:email)
+  should validate_presence_of(:password)
+  should validate_length_of(:first_name).is_at_most(50)
+  should validate_length_of(:last_name).is_at_most(75)
+  should validate_length_of(:email).is_at_most(255)
+  should validate_length_of(:password).is_at_least(8)
 end
