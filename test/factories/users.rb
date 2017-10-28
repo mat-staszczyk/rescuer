7ec629f9e88fccf8f0267fa331469cc661c45749
sleaz0id <mat.staszczyk@gmail.com>
2017-10-28 21:46:05 +0200

    User factory added

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name 'John'
+    last_name 'Malkovic'
+    sequence :email { |n| "email#{n}@email.com" }
+    password 'password'
+    password_confirmation 'password'
+    confirmed_at Time.zone.now - 1.hour
+    confirmation_sent_at Time.zone.now - 2.hours
   end
 end
