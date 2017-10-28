adda1d177553e0655de6c41ec513e162a79beefa
sleaz0id <mat.staszczyk@gmail.com>
2017-10-28 22:40:45 +0200

    Factories added and db:seed setup

--- test/factories/help_requests.rb
+++ test/factories/help_requests.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :help_request do
-    title "MyString"
-    description "MyText"
-    references ""
+    title "Monster alert"
+    description "Monster alert"
+    association :author, factory: :user
+    
+    factory :help_request_with_rescuers do
+      after(:create) do |request|
+        3.times { request.rescuers << FactoryBot.create(:user) } 
+      end
+    end
   end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,10 +2,9 @@ FactoryBot.define do
   factory :user do
     first_name 'John'
     last_name 'Malkovic'
+    birth_date Faker::Date.birthday(18, 65)
     sequence :email { |n| "email#{n}@email.com" }
     password 'password'
     password_confirmation 'password'
-    confirmed_at Time.zone.now - 1.hour
-    confirmation_sent_at Time.zone.now - 2.hours
   end
 end
