3ecdeef1edc40be0ee57fe19481ab763f4b41625
sleaz0id <mat.staszczyk@gmail.com>
2017-10-29 02:48:24 +0200

    Help request helpers added

--- app/helpers/help_requests_helper.rb
+++ app/helpers/help_requests_helper.rb
@@ -1,2 +1,13 @@
 module HelpRequestsHelper
+  def is_user_request?
+    @help_request.author == current_user
+  end
+
+  def is_draft_request?
+    @help_request.state == 'draft'
+  end
+
+  def is_active_request?
+    @help_request.state == 'active'
+  end
 end
