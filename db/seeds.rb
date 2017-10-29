9a8b76d2e1d54a8be9d8824037750eb44d41d3ab
jerryskye <jarek@siedlarz.com>
2017-10-29 06:39:49 +0100

    fixed default behavior for creating help requests with FactoryBot

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 HelpRequest.destroy_all
 User.destroy_all
 puts "Destroyed all users and help requests"
-FactoryBot.create(:help_request_with_rescuers)
+FactoryBot.create(:help_request_with_rescuers, state: 'active')
 puts "Help request with rescuers created"
-3.times { FactoryBot.create(:help_request) }
+3.times { FactoryBot.create(:help_request, state: 'active') }
 puts "3 standalone help requests created"
