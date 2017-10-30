88f29899568be6dac58d62666bd50b7d0c54585b
sleaz0id <mat.staszczyk@gmail.com>
2017-10-30 18:26:36 +0100

    Polish request names added to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 HelpRequest.destroy_all
 User.destroy_all
 puts "Destroyed all users and help requests"
-FactoryBot.create(:help_request_with_rescuers, state: 'active')
+
+help_requests = [
+  'Pomoc w naprawie roweru',
+  'Wniesienie kanapy na 4 piętro',
+  'Malowanie pokoju',
+  'Pomoc w dotarciu na pogrzeb'
+]
+help_requests.each do |request|
+  FactoryBot.create(:help_request, title: request, state: 'active')
+end
+puts "#{help_requests.count} standalone help requests created"
+
+FactoryBot.create(:help_request_with_rescuers, title: 'Organizacja lokalnego kiermaszu', state: 'active')
 puts "Help request with rescuers created"
-3.times { FactoryBot.create(:help_request, state: 'active') }
-puts "3 standalone help requests created"
